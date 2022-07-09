7e050bff60f1a6e41e2c6d51b67dd8c387fa56f9
MoussaNgongo <alvaro_ac3@outlook.es>
2022-07-09 16:12:42 +0200

    Ejercicio 3
    Utilizando un bucle, mostrar la suma y la media de los números introducidos hasta introducir un numero negativo y ahí mostrar el resultado.

--- 3/3.js
+++ 3/3.js
@@ -1,14 +1,18 @@
-let suma=0;
+//Utilizando un bucle, mostrar la suma y la media de los numeros introducidos hasta introducir
+//un numero negativo y ahí mostrar el resultado.
+
+
+let suma = 0;
 let introducido = prompt("Dime num", "");
-let numeroNumeros=0;
+let numeroNumeros = 0;
 
 
-while (introducido>=0) {
-    suma+=Number(introducido);
-    numeroNumeros+=1;
+while (introducido >= 0) {
+    suma += Number(introducido);
+    numeroNumeros += 1;
     introducido = prompt("Dime num", "");
 
 }
 
 document.getElementById("suma").innerText += " " + suma.toString();
-document.getElementById("media").innerText += " " + (suma/numeroNumeros).toString();
+document.getElementById("media").innerText += " " + (suma / numeroNumeros).toString();
